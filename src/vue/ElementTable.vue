8a256c3c8c01e4cf830804d23fa2371b4cd7457b
japlus <601286825@qq.com>
2024-04-24 22:16:38 +0800

    cannot use element

--- src/vue/ElementTable.vue
+++ src/vue/ElementTable.vue
@@ -2,27 +2,40 @@
   <div>
     <h2>element-plus table</h2>
     <ul>
-      <li>beta版本用不了</li>
+      <li>固定列用不了</li>
+      <li>需要设置宽度</li>
     </ul>
-    <!-- <TableV2 row-key="id" :height="600" :columns :data="tableData"></TableV2> -->
+    <TableV2
+      row-key="id"
+      fixed
+      :height="600"
+      :max-height="600"
+      :width="800"
+      :columns
+      :data="tableData"
+    ></TableV2>
   </div>
 </template>
 
 <script setup>
-// import { TableV2 } from 'element-plus';
-// import { computed } from 'vue';
-// import { tableColumns, tableData } from '../stk-table/props.js';
-// import 'element-plus/theme-chalk/index.css';
-
-// const columns = computed(() =>
-//   tableColumns.map(it => ({
-//     dataKey: it.dataIndex,
-//     title: it.title,
-//     width: it.width,
-//     fixed: it.fixed,
-//     align: it.align,
-//     titleAlign: it.headerAlign,
-//   })),
-// );
+import { TableV2, TableV2FixedDir } from 'element-plus';
+import { computed } from 'vue';
+import { tableColumns, tableData } from '../stk-table/props.js';
+import 'element-plus/theme-chalk/index.css';
+const fixedMap = {
+  left: TableV2FixedDir.LEFT,
+  right: TableV2FixedDir.RIGHT,
+};
+const columns = computed(() =>
+  tableColumns.map(it => ({
+    key: it.dataIndex,
+    dataKey: it.dataIndex,
+    title: it.title,
+    width: it.width,
+    // fixed: fixedMap[it.fixed],
+    // align: it.align,
+    // titleAlign: it.headerAlign,
+  })),
+);
 // Your script code here
 </script>
