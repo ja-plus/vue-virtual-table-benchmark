f27de39e12ae39cdd0715ddd2889490adf1cb865
JA+ <601286825@qq.com>
2025-08-18 15:48:34 +0800

    update

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -57,6 +57,7 @@ import VTable from './VTable.vue';
     </div>
     <div>
       <h2>VTable</h2>
+      <p> 提示：与StkTable 同比性能时要移除上面其他表格。页面元素过多会影响表格滚动性能。</p>
       <VTable></VTable>
     </div>
   </div>
